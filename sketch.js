2201d8adaaab03a589eb1df24ecfe3eca0358b02
MRL786-786 <72897761+MRL786-786@users.noreply.github.com>
2021-05-20 09:14:00 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var manqabat;
+var m2
+
+
+function preload(){
+  
+  BG= loadImage("Screenshot (36).png")
+  
+ manqabat = loadSound("Ajal Allah ho Zahurak - Arabi Munqabat.mp3")
+  
+ m2 = loadSound("Abad-Wallah-Ma-Ninsa-Hussaina-Ba.mp3")
+}
+
+function setup() {
+  createCanvas(width,height);
+  
+}
+
+function draw() {
+  background(BG);
+  
+   
+  if(keyIsDown(49)){
+    manqabat.play();
+  }
+  
+  if(keyIsDown(50)){
+    m2.play();
+  }
+}
